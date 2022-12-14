c10809caf24a1a68fba5bea5d8196af8d5a37166
jsus marcano <jesusjaviermarcanomora.2005@gmail.com>
2022-12-14 17:15:22 -0400

    create student service

--- src/contracts/student.ts
+++ src/contracts/student.ts
@@ -1,12 +1,14 @@
 import { z } from 'zod'
 
-const id = z.coerce.number().int()
+const studentId = z.coerce.number().int()
 export const studentDTO = z.object({
-  id,
+  id: studentId.optional(),
   name: z.string().min(2).max(100),
   lastName: z.string().min(2).max(100),
   contactNumber: z.string().min(7).max(30),
-  bornDate: z.string().datetime(),
+  bornDate: z.preprocess((arg) => {
+    if(typeof arg === "string" || arg instanceof Date) return new Date(arg)
+  }, z.date()),
   studyLevel: z.string().max(200),
 })
 
---
+++ src/services/student.service.ts
@@ -0,0 +1,33 @@
+import { StudentDTO, StudentRepository } from "../contracts/student.js";
+import { AppDataSource } from "../db/data-source.js";
+import { Student } from "../db/entity/Student.js";
+import { serviceErr } from "../errors/ServiceError.js";
+
+export class StudentService implements StudentRepository {
+  private repository = AppDataSource.getRepository(Student)
+
+  async create(data: StudentDTO): Promise<StudentDTO> {
+    const newStudent = await this.repository.save(data)
+    return newStudent
+  }
+
+  async find(): Promise<StudentDTO[]> {
+    const students = await this.repository.find()
+    return students
+  }
+  async findOne(id: number): Promise<StudentDTO> {
+    const student = await this.repository.findOneBy({id})
+    if(!student) {
+      throw serviceErr(404, "Student not found.")
+    }
+    return student
+  }
+  async update(id: number, data: StudentDTO) {
+    await this.findOne(id)
+    await this.repository.update(id, data)
+  }
+  async delete(id: number): Promise<void> {
+    await this.findOne(id)
+    await this.repository.delete(id)
+  }
+}
