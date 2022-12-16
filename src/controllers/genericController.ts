57035fabf35e21683bab6d33856b828f7dc7f40c
jsus marcano <jesusjaviermarcanomora.2005@gmail.com>
2022-12-15 23:24:04 -0400

    create generic controller

---
+++ src/controllers/genericController.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import { z, ZodSchema } from "zod";
+import { Repository } from '../contracts/repository.js'
+import { createRoute } from "./index.js";
+
+export class CRUDController<model extends ZodSchema> {
+  schema: model
+  repository: Repository<model>
+  path = '/'
+  idParser = z.coerce.number().int()
+  constructor(repository: Repository<model>, schema: model) {
+    this.repository = repository
+    this.schema = schema
+  }
+  getRoutes() {
+    // TODO: convert routes array to map
+    return [
+      createRoute("get", "/", this.read),
+      createRoute("get", "/:id", this.readOne),
+      createRoute("post", "/", this.create),
+      createRoute("patch", "/:id", this.update),
+      createRoute("delete", "/:id", this.delete),
+    ]
+  }
+  /**
+   * Returns all instances of the model 
+   */
+  async read(req: Request, res: Response) {
+    return this.repository.find()
+  }
+  /**
+   * Returns an instance of the model by 
+   * the identifier specified in the url parameter
+   */
+  async readOne(req: Request, res: Response) {
+    const id = this.idParser.parse(req.params.id)
+    return this.repository.findOne(id)
+  }
+  /**
+   * Create an instance of the model
+   */
+  async create(req: Request, res: Response) {
+    const data = this.schema.parse(req.body)
+    const instance = this.repository.create(data)
+    res.status(201)
+    return instance
+  }
+  /**
+   * Update an instance of the model
+   */
+  async update(req: Request, res: Response) {
+    const id = this.idParser.parse(req.params.id)
+    const data = this.schema.parse(req.body)
+    this.repository.update(id, data)
+    res.sendStatus(200)
+  }
+  /**
+   * Deletes an instance of the model
+   */
+  async delete(req: Request, res: Response) {
+    const id = this.idParser.parse(req.params.id)
+    this.repository.delete(id)
+    res.sendStatus(200)
+  }
+}
